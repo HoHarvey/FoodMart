122001cbaf42658ea57de0c49529ebb1bba8fd20
HoHarvey <harvey_1997@hotmail.ca>
2025-11-21 13:55:18 -0500

    added comments for clarity of food model

--- app/src/main/java/com/hohar/foodmart/model/Food.kt
+++ app/src/main/java/com/hohar/foodmart/model/Food.kt
@@ -2,11 +2,22 @@ package com.hohar.foodmart.model
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Data model for a food entity in the app
+ *
+ * uuid is the unique identifier for the food
+ * title is the name of the food
+ * price is the price of the food
+ * category_uuid is the unique identifier for the category of the food
+ * image_url is the URL for the image of the food
+ * nullable for API compatibility
+ */
+
 @Serializable
 data class Food (
     val uuid: String,
     val title: String? = null,
     val price: Float? = null,
     val category_uuid: String? = null,
-    val image_url: String?
+    val image_url: String? = null
 )
