142666bd2be928085019d2657c479551daac4ffe
HoHarvey <harvey_1997@hotmail.ca>
2025-11-22 15:06:44 -0500

    added test for retry loading

--- app/src/test/java/com/hohar/foodmart/viewmodel/FoodMartViewModelTest.kt
+++ app/src/test/java/com/hohar/foodmart/viewmodel/FoodMartViewModelTest.kt
@@ -37,5 +37,19 @@ class FoodMartViewModelTest {
         assertTrue(viewModel.uiState.value is UiState.Loading)
     }
 
+    @Test
+    fun `retry sets state to Loading then Success`() = runTest {
+        val viewModel = FoodMartViewModel()
 
+        // Move to a finished state first
+        advanceUntilIdle()
+
+        // Act
+        viewModel.retry()
+
+        // Assert: It should briefly be loading (though in a test this happens instantly
+        // unless we pause the dispatcher), and then eventually success.
+        advanceUntilIdle()
+        assertTrue(viewModel.uiState.value is UiState.Success)
+    }
 }
