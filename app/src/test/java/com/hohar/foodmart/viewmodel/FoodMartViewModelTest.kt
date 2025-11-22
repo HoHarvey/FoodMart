7f3c8863871a8c35b4156fce7ef6407a26bb196f
HoHarvey <harvey_1997@hotmail.ca>
2025-11-22 14:52:36 -0500

    added test for viewmodel initially loading

--- app/src/test/java/com/hohar/foodmart/viewmodel/FoodMartViewModelTest.kt
+++ app/src/test/java/com/hohar/foodmart/viewmodel/FoodMartViewModelTest.kt
@@ -1,12 +1,15 @@
 package com.hohar.foodmart.viewmodel
 
+import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class FoodMartViewModelTest {
@@ -20,6 +23,13 @@ class FoodMartViewModelTest {
     @After
     fun tearDown() {
         Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `uiState is initially Loading`() = runTest {
+        val viewModel = FoodMartViewModel()
+
+        assertTrue(viewModel.uiState.value is UiState.Loading)
     }
 
 }
